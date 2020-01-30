30d472acd5099fcecf77311777bbba2a127f7245
liuzhuguan <1424079837@qq.com>
2020-01-30 15:58:46 +0800

    修改

--- git_test/src/Git_test.java
+++ git_test/src/Git_test.java
@@ -2,4 +2,9 @@ public class Git_test {
     public static void main(String[] args) {
         System.out.println("测试一下");
     }
+
+    public int array(int a) {
+        a = a*2;
+        return a;
+    }
 }
