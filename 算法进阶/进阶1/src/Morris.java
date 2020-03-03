618c54dd06a46afee684c0287d40391179d2a46b
liuzhuguan <1424079837@qq.com>
2020-03-03 13:46:12 +0800

    完善可Morris遍历

--- 算法进阶/进阶1/src/Morris.java
+++ 算法进阶/进阶1/src/Morris.java
@@ -19,14 +19,75 @@ public class Morris {
     }
 
     public static void morrisPre(Node head) {
-
+        if (head == null) {
+            return;
+        }
+        Node current = head;
+        Node mosRight = null;
+        while (current != null) {
+            mosRight = current.left;
+            if (mosRight != null) {
+                while (mosRight.right != null && mosRight.right != current) {
+                    mosRight = mosRight.right;
+                }
+                if (mosRight.right == null) {
+                    mosRight.right = current;
+                    System.out.print(current.value + " ");
+                    current = current.left;
+                    continue;
+                } else {
+                    mosRight.right = null;
+                }
+            } else {
+                System.out.print(current.value + " ");
+            }
+            current = current.right;
+        }
+        System.out.println();
     }
 
     public static void morrisIn(Node head) {
-
+        if (head == null) {
+            return;
+        }
+        Node current = head;
+        Node mosRight = null;
+        while (current != null) {
+            mosRight = current.left;
+            if (mosRight != null) {
+                while (mosRight.right != null && mosRight.right != current) {
+                    mosRight = mosRight.right;
+                }
+                if (mosRight.right == null) {
+                    mosRight.right = current;
+                    current = current.left;
+                    continue;
+                } else {
+                    mosRight.right = null;
+                }
+            }
+            System.out.print(current.value + " ");
+            current = current.right;
+        }
+        System.out.println();
     }
 
-    public static void morrisPos(Node head) {
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        Node node = new Node(2);
+        Node node1 = new Node(3);
+        Node node2 = new Node(4);
+        Node node3 = new Node(5);
+        Node node4 = new Node(6);
+        Node node5 = new Node(7);
 
+        head.left = node;
+        head.right = node1;
+        head.left.left = node2;
+        head.left.right = node3;
+        head.right.left = node4;
+        head.right.right = node5;
+        morrisIn(head);
+        morrisPre(head);
     }
 }
